2fc8049edf82d7d9ef15dbf8b7d4b1dff18b6874
krish gupta <159681672+Krishgupta8889@users.noreply.github.com>
2025-12-26 16:02:51 +0530

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -6,8 +6,8 @@ export const PORTFOLIO_ITEMS: PortfolioItem[] = [
   { id: 2, url: 'https://res.cloudinary.com/dbmikomtn/image/upload/v1766743624/WhatsApp_Image_2025-12-25_at_11.50.33_PM_k7n0um.jpg', category: 'Portraits', title: 'together' },
   { id: 4, url: 'https://res.cloudinary.com/dbmikomtn/image/upload/v1766743624/WhatsApp_Image_2025-12-25_at_8.31.42_PM_1_u93r82.jpg', category: 'Weddings', title: 'not perfect just real' },
   { id: 5, url: 'https://res.cloudinary.com/dbmikomtn/image/upload/v1766743624/WhatsApp_Image_2025-12-25_at_8.31.35_PM_mzsgno.jpg', category: 'Portraits', title: 'elegance' },
-  { id: 7, url: 'https://picsum.photos/600/600?random=7', category: 'Weddings', title: 'Reception Glow' },
-  { id: 8, url: 'https://picsum.photos/800/700?random=8', category: 'Portraits', title: 'Classic Elegance' },
+  { id: 7, url: 'https://res.cloudinary.com/dbmikomtn/image/upload/v1766744951/WhatsApp_Image_2025-12-25_at_8.45.56_PM_ccxfrr.jpg', category: 'Weddings', title: 'nayi shuruaat' },
+  { id: 8, url: 'https://res.cloudinary.com/dbmikomtn/image/upload/v1766745115/WhatsApp_Image_2025-12-25_at_11.50.20_PM_majuru.jpg', category: 'Portraits', title: 'forevers' },
 ];
 
 export const PRICING_PLANS: PricingPlan[] = [
